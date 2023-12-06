9242ef525fe18fc842ae39bbb4433e87508a986d
brmr <277226+brmr@users.noreply.github.com>
2023-12-05 23:37:07 -0500

    Update calibration.py
    Working version

--- calibration.py
+++ calibration.py
@@ -1,17 +1,23 @@
+import re
+
+num_words = {'one':1,'two':2,'three':3,
+             'four':4,'five':5,'six':6,
+             'seven':7,'eight':8,'nine':9}
+
+#p = re.compile(r'\d', re.IGNORECASE)
+p = re.compile(r'(?=(\d|'+'|'.join(num_words)+'))', re.IGNORECASE)
+
 calibration_sum = 0
 
-def find_digit(line):
-  return 0
-
-def find_first(line):
-  return 0
-
-def find_last(line):
-  return 0
+def find_digits(line):
+  m = p.findall(line)
+  if m[0] in num_words:
+    m[0] = num_words[m[0]]
+  if m[-1] in num_words:
+    m[-1] = num_words[m[-1]]
+  return (str(m[0]) + str(m[-1]))
 
 with open('trebuchet_calibration.txt', 'r') as f:
   for line in f:
-    first_digit = find_first(line)
-    second_digit = find_last(line)
-    calibration_sum += int(str(first_digit) + str(second_digit))
+    calibration_sum += int(find_digits(line))
 print(f'The sum of all calibration values is {calibration_sum}.')
